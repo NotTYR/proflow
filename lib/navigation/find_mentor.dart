ab2c0ff51cb64813e0b9fe8651847e07a743ac23
Tan You Ren <105924972+NotTYR@users.noreply.github.com>
2023-04-09 09:50:25 +0800

    asdf

--- lib/navigation/find_mentor.dart
+++ lib/navigation/find_mentor.dart
@@ -1,38 +1,11 @@
-import 'package:ProFlow/navigation/proposal_page.dart';
 import 'package:flutter/material.dart';
-
-import '../appbar.dart';
+import 'package:gsheets/gsheets.dart';
 
 class FindMentor extends StatelessWidget {
   const FindMentor({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: ProFlowAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: const Text('You have not proposed any projects.'),
-              margin: const EdgeInsets.only(bottom: 15.0),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const ProposalPage();
-                    },
-                  ),
-                );
-              },
-              child: const Text('Propose one now'),
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
