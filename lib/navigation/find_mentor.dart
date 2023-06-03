0f81e0588e1b41fe1b9c143b2593915efe116d44
Tan You Ren <105924972+NotTYR@users.noreply.github.com>
2023-06-03 10:57:27 +0800

    js slay

--- lib/navigation/find_mentor.dart
+++ lib/navigation/find_mentor.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_js/flutter_js.dart';
+import 'package:flutter_js/quickjs/ffi.dart';
 import 'sheets_api.dart';
 import 'package:http/http.dart' as http;
 
 class IspMentorListRequest {
   static VisitIsp() async {
+    final JavascriptRuntime javascriptRuntime = getJavascriptRuntime();
     dynamic getcookie = await http.get(Uri.https('isphs.hci.edu.sg'));
     dynamic rngcode = RegExp('z" value="(.*?)"').firstMatch(getcookie.body)![1];
     while (RegExp('.*[E].*').hasMatch(rngcode!)) {
@@ -14,6 +18,7 @@ class IspMentorListRequest {
     final cookie = getcookie.headers["set-cookie"]?.split("; ")[0];
     if (cookie != null) {
       print(cookie);
+
       final code = 'code';
       final isp = await http.post(Uri.https('isphs.hci.edu.sg', 'pwd_auth.asp'),
           headers: {'Connection': 'close', 'Cookie': cookie},
@@ -23,6 +28,12 @@ class IspMentorListRequest {
       print(htmlstr);
     }
   }
+}
+
+Future<String> mdhash(JavascriptRuntime javascriptRuntime, i, Y, W, U) async {
+  String md5 = await rootBundle.loadString('lib/mdhash.js');
+  final result = javascriptRuntime.evaluate(md5 + """MD5($i, $Y, $W, $U)""");
+  return result.toString();
 }
 
 class FindMentor extends StatelessWidget {
