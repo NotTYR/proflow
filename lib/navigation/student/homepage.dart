706fe3a25679c68c53508af753a21dec842cc0a3
WENYANG YE <181888Y@student.hci.edu.sg>
2023-08-10 18:52:21 +0800

    added homepage help

--- lib/navigation/student/homepage.dart
+++ lib/navigation/student/homepage.dart
@@ -1,16 +1,14 @@
 import 'package:ProFlow/extensions.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:ProFlow/navigation/student/create%20group/create_group.dart';
-import 'package:ProFlow/navigation/student/forum/view.dart';
-import 'package:ProFlow/navigation/student/my%20project/modules/home/view.dart';
+import 'package:ProFlow/navigation/student/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ProFlow/guest_page.dart';
 import 'package:ProFlow/navigation/student/chat/chat_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:ProFlow/main.dart';
+import 'package:ProFlow/navigation/student/forum/view.dart';
+import 'package:ProFlow/navigation/student/my%20project/modules/home/view.dart';
 
 class StudentPage extends StatefulWidget {
   const StudentPage({super.key});
@@ -20,98 +18,122 @@ class StudentPage extends StatefulWidget {
 }
 
 class _StudentPageState extends State<StudentPage> {
+  final Object _helpHeroTag = UniqueKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: SvgPicture.asset('assets/proflow.svg'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: SvgPicture.asset('assets/proflow.svg'),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.5,
+            margin: EdgeInsets.only(
+              top: 4.0.hp,
+              bottom: 3.0.hp,
             ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              margin: EdgeInsets.only(
-                top: 4.0.hp,
-                bottom: 3.0.hp,
-              ),
-              child: FittedBox(
-                child: Text(
-                  'ProFlow',
-                  style: TextStyle(
-                    color: Color(0xFF3874CB),
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                  ),
+            child: FittedBox(
+              child: Text(
+                'ProFlow',
+                style: TextStyle(
+                  color: Color(0xFF3874CB),
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
                 ),
               ),
             ),
-            Container(
-                width: MediaQuery.of(context).size.width * 0.6,
-                margin: EdgeInsets.only(
-                  bottom: 6.0.hp,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.6,
+            margin: EdgeInsets.only(
+              bottom: 6.0.hp,
+            ),
+            child: const FittedBox(
+              child: Text(
+                'Experience Redefined',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 187, 187, 187),
+                  fontStyle: FontStyle.italic,
                 ),
-                child: const FittedBox(
-                    child: Text(
-                  'Experience Redefined',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 187, 187, 187),
-                    fontStyle: FontStyle.italic,
-                  ),
-                ))),
-            Padding(
-              padding: EdgeInsets.only(bottom: 3.0.hp),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    child: FeatureButton(
-                        buttonName: 'Forum',
-                        buttonIcon: Icons.question_answer,
-                        buttonRoute: ForumExp()),
-                  ),
-                  SizedBox(
-                    child: FeatureButton(
-                        buttonName: 'My Project',
-                        buttonIcon: Icons.list_alt_rounded,
-                        buttonRoute: MyProjects()),
-                  ),
-                  SizedBox(
-                    child: FeatureButton(
-                        buttonName: 'Chat',
-                        buttonIcon: Icons.chat,
-                        buttonRoute: Chat()),
-                  ),
-                ],
               ),
             ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            try {
-              await FirebaseAuth.instance.signOut();
-              await GoogleSignIn().signOut();
-              Get.to(() => GuestPage(), transition: Transition.noTransition);
-            } catch (e) {
-              print('homepaeg.dart error');
-              print(e.toString());
-            }
-          },
-          child: Icon(Icons.logout_outlined),
-        ));
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 3.0.hp),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  child: FeatureButton(
+                    buttonName: 'Forum',
+                    buttonIcon: Icons.question_answer,
+                    buttonRoute: ForumExp(),
+                  ),
+                ),
+                SizedBox(
+                  child: FeatureButton(
+                    buttonName: 'My Project',
+                    buttonIcon: Icons.list_alt_rounded,
+                    buttonRoute: MyProjects(),
+                  ),
+                ),
+                SizedBox(
+                  child: FeatureButton(
+                    buttonName: 'Chat',
+                    buttonIcon: Icons.chat,
+                    buttonRoute: Chat(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => OnboardingPage()));
+            },
+            child: Icon(Icons.help_outline_outlined),
+            heroTag: _helpHeroTag,
+          ),
+          FloatingActionButton(
+            onPressed: () async {
+              try {
+                await FirebaseAuth.instance.signOut();
+                await GoogleSignIn().signOut();
+                Get.to(() => GuestPage(), transition: Transition.noTransition);
+              } catch (e) {
+                print('homepaeg.dart error');
+                print(e.toString());
+              }
+            },
+            child: Icon(Icons.logout_outlined),
+          ),
+        ],
+      ),
+    );
   }
 }
 
 class FeatureButton extends StatelessWidget {
-  FeatureButton(
-      {super.key,
-      required this.buttonName,
-      required this.buttonIcon,
-      required this.buttonRoute});
+  FeatureButton({
+    super.key,
+    required this.buttonName,
+    required this.buttonIcon,
+    required this.buttonRoute,
+  });
+
   final String buttonName;
   final IconData buttonIcon;
   final Widget buttonRoute;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -121,8 +143,6 @@ class FeatureButton extends StatelessWidget {
           RawMaterialButton(
             onPressed: () {
               Get.to(() => buttonRoute, transition: Transition.noTransition);
-              // Navigator.of(context)
-              //     .push(MaterialPageRoute(builder: (context) => buttonRoute));
             },
             fillColor: Color(0xFF3874CB),
             child: Icon(
@@ -143,4 +163,10 @@ class FeatureButton extends StatelessWidget {
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: StudentPage(),
+  ));
 }
